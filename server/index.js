3e23d5e239983835cd4fa9962710a18d12ad8dc2
Anastasia Shvydko <m.a.34@yandex.ru>
2025-12-15 16:56:22 -0500

    fix index.js

--- server/index.js
+++ server/index.js
@@ -78,6 +78,8 @@ app.use("/api/slots", require("./routes/slots"));
 app.use("/api/bookings", require("./routes/bookings"));
 
 // // Start the server
-app.listen(5000, () => {
-  console.log('🚀 Server is running at http://localhost:5000');
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
